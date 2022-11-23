9d2fa4fc93d3fc31ecb89ce58598a466656c2f98
rhyloo <jorge2@uma.es>
2022-11-23 17:15:25 +0100

    Código añadido al template

--- Time_Verificator/Time_Verificator.srcs/sim_1/new/Test_Bench.vhd
+++ Time_Verificator/Time_Verificator.srcs/sim_1/new/Test_Bench.vhd
@@ -22,22 +22,56 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity Test_Bench is
 --  Port ( );
 end Test_Bench;
 
 architecture Behavioral of Test_Bench is
 
+  component FF_D
+    generic(T_SU :    Time := 1 ns; T_H : Time := 1 ns; T_W : Time := 1 ns);
+    port(D       : in std_logic; q : out std_logic; clk : in std_logic);
+  end Component FF_D;
+
+  signal D_interno, q_interno : STD_LOGIC := 'U';
+  signal clk_interno          : STD_LOGIC := '0';
+  constant semiperiodo        : time      := 10 ns;
+
 begin
 
+  DUT : FF_D
+    generic map (
+      T_SU => 1 ns, -- tiempo de setup
+      T_H  => 1 ns, -- tiempo de hold
+      T_W  => 1 ns) -- ancho del pulso
+    port map(
+      D   => D_interno,
+      q   => q_interno,
+      clk => clk_interno);
 
+  clock_gen : process (clk_interno) is
+  begin
+    if clk_interno = '0' then
+      clk_interno <= '1' after semiperiodo,
+                     '0' after 2*semiperiodo;
+    end if;
+  end process clock_gen;
+
+  Data : process
+  begin
+    -- Pulso correcto
+    q_interno <= '0';
+    wait for 9 ns;
+    q_interno <= '1';
+    wait for 2 ns;
+    q_interno <= '0';
+
+    wait for 19 ns;
+
+    -- Pulso incorrecto
+    q_interno <= '1';
+    wait for 1 ns;
+    q_interno <= '0';
+    wait;
+    end process;
 end Behavioral;
