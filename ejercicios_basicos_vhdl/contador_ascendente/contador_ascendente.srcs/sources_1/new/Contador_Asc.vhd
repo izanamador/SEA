039ec4b3b64c528f5bd3519f92e02d31c5c6d5d6
izanamador <izan.amador@gmail.com>
2022-11-26 17:01:00 +0100

    up counter project created

---
+++ ejercicios_basicos_vhdl/contador_ascendente/contador_ascendente.srcs/sources_1/new/Contador_Asc.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 26.11.2022 16:59:41
+-- Design Name: 
+-- Module Name: Contador_Asc - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Contador_Asc is
+--  Port ( );
+end Contador_Asc;
+
+architecture Behavioral of Contador_Asc is
+
+begin
+
+
+end Behavioral;
