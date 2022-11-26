ac3b8fc1adf183994c0723ce356dcec4c7b38491
rhyloo <jorge2@uma.es>
2022-11-26 17:01:54 +0100

    MuxV_2a1 Test Bench escrito

---
+++ ejercicios_basicos/Modelos/Reg_Des.vhd
@@ -0,0 +1,50 @@
+----------------------------------------------------------------------------------
+-- Company:
+-- Engineer:
+--
+-- Create Date: 26.11.2022 16:14:50
+-- Design Name:
+-- Module Name: MuxV_2a1 - Behavioral
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+----------------------------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity Reg_Des is
+  generic(n : integer := 8);
+  port(
+    d          : in  std_logic;
+    q          : out std_logic_vector(n-1 downto 0);
+    reset, des : in  std_logic;
+    clk        : in  std_logic);
+end Reg_Des;
+
+architecture A of Reg_Des is
+  signal temp : std_logic_vector(n-1 downto 0);
+begin
+  process(clk, reset)
+  begin
+    if reset = '1' then
+      temp <= (others => '0');
+    elsif rising_edge(clk) then
+      if des = '1' then
+        for i in temp'high downto 1 loop
+          temp(i) <= temp(i-1);
+        end loop;
+        temp(0) <= d;
+      end if;
+    end if;
+  end process;
+  q <= temp;
+end A;
--- ejercicios_basicos/Modelos/Valid/MuxV_2a1.vhd
+++ ejercicios_basicos/Modelos/Valid/MuxV_2a1.vhd
@@ -18,26 +18,27 @@
 -- 
 ----------------------------------------------------------------------------------
 
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity MuxV_2a1 is
---  Port ( );
+  generic(
+    n : integer := 8);
+  port(
+    x0  : in  std_logic_vector(n-1 downto 0);
+    x1  : in  std_logic_vector(n-1 downto 0);
+    sel : in  std_logic;
+    y   : out std_logic_vector(n-1 downto 0));
 end MuxV_2a1;
 
 architecture Behavioral of MuxV_2a1 is
-
 begin
-
-
+  process(sel, x0, x1)
+  begin
+    if sel = '1' then
+      y <= x1;
+    else
+      y <= x0;
+    end if;
+  end process;
 end Behavioral;
