e369dd34149fec6b61feb347d4d45c415f2d4c57
rhyloo <jorge2@uma.es>
2022-11-26 17:07:21 +0100

    Mux2a1 en la carpeta correcta, lista para simulación

--- ejercicios_basicos_vhdl/multiplexor2a1/multiplexor2a1.srcs/sources_1/new/MuxV_2a1.vhd
+++ ejercicios_basicos_vhdl/multiplexor2a1/multiplexor2a1.srcs/sources_1/new/MuxV_2a1.vhd
@@ -1,43 +1,44 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 26.11.2022 16:42:44
--- Design Name: 
+-- Company:
+-- Engineer:
+--
+-- Create Date: 26.11.2022 16:14:50
+-- Design Name:
 -- Module Name: MuxV_2a1 - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
--- 
+--
 ----------------------------------------------------------------------------------
 
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity MuxV_2a1 is
---  Port ( );
+  generic(
+    n : integer := 8);
+  port(
+    x0  : in  std_logic_vector(n-1 downto 0);
+    x1  : in  std_logic_vector(n-1 downto 0);
+    sel : in  std_logic;
+    y   : out std_logic_vector(n-1 downto 0));
 end MuxV_2a1;
 
 architecture Behavioral of MuxV_2a1 is
-
 begin
-
-
+  process(sel, x0, x1)
+  begin
+    if sel = '1' then
+      y <= x1;
+    else
+      y <= x0;
+    end if;
+  end process;
 end Behavioral;
