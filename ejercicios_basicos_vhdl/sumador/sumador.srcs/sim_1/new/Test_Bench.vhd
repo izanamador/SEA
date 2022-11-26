bf8d04303af6e0f54aaa8d0798aff3c089f0cb64
rhyloo <jorge2@uma.es>
2022-11-26 17:54:43 +0100

    Sum code completed

--- ejercicios_basicos_vhdl/sumador/sumador.srcs/sim_1/new/Test_Bench.vhd
+++ ejercicios_basicos_vhdl/sumador/sumador.srcs/sim_1/new/Test_Bench.vhd
@@ -1,35 +1,27 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 26.11.2022 16:48:00
--- Design Name: 
+-- Company:
+-- Engineer:
+--
+-- Create Date: 23.11.2022 16:50:19
+-- Design Name:
 -- Module Name: Test_Bench - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
--- 
+--
 ----------------------------------------------------------------------------------
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use ieee.numeric_std.all;
 
 entity Test_Bench is
 --  Port ( );
@@ -37,7 +29,44 @@ end Test_Bench;
 
 architecture Behavioral of Test_Bench is
 
+  component Sum
+    generic(
+      n : integer := 8);
+    port(x : in  std_logic_vector(n-1 downto 0);
+         y : in  std_logic_vector(n-1 downto 0);
+         s : out std_logic_vector(n-1 downto 0));
+  end Component Sum;
+
+  constant n : integer := 3;
+  signal x_interno, y_interno, s_interno : std_logic_vector(n-1 downto 0) := (others => 'U');
+
 begin
 
+  DUT : Sum
+    generic map (n)
+    port map(
+      x  => x_interno,
+      y  => y_interno,
+      s  => s_interno);
 
+  test: process
+    constant maximo : integer := 8;
+  begin
+
+    for item in 0 to maximo-1 loop
+
+      x_interno <= std_logic_vector(to_unsigned(item,n));
+
+      for item2 in 0 to maximo-1 loop
+
+        y_interno <= std_logic_vector(to_unsigned(item2,n));
+        wait for 5 ns;
+
+      end loop;
+
+      -- wait for 5 ns;
+
+    end loop;
+
+  end process;
 end Behavioral;
--- ejercicios_basicos_vhdl/sumador/sumador.srcs/sources_1/new/Sum.vhd
+++ ejercicios_basicos_vhdl/sumador/sumador.srcs/sources_1/new/Sum.vhd
@@ -1,43 +1,36 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 26.11.2022 16:47:11
--- Design Name: 
--- Module Name: Sum - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
+-- Company:
+-- Engineer:
+--
+-- Create Date: 26.11.2022 16:14:50
+-- Design Name:
+-- Module Name: MuxV_2a1 - Behavioral
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
--- 
+--
 ----------------------------------------------------------------------------------
 
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity Sum is
---  Port ( );
+  generic(
+    n : integer := 8);
+  port(x : in  std_logic_vector(n-1 downto 0);
+       y : in  std_logic_vector(n-1 downto 0);
+       s : out std_logic_vector(n-1 downto 0));
 end Sum;
 
-architecture Behavioral of Sum is
-
+architecture Simple of Sum is
 begin
-
-
-end Behavioral;
+  s <= std_logic_vector(unsigned(x) + unsigned(y));
+end Simple;
