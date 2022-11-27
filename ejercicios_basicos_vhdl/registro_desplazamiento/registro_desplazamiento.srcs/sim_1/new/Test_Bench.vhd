f66f4c51a77e4bb4530013e45fd854c55ba618ae
rhyloo <jorge2@uma.es>
2022-11-27 17:03:13 +0100

    Reg_Des simulation wrotten

--- ejercicios_basicos_vhdl/registro_desplazamiento/registro_desplazamiento.srcs/sim_1/new/Test_Bench.vhd
+++ ejercicios_basicos_vhdl/registro_desplazamiento/registro_desplazamiento.srcs/sim_1/new/Test_Bench.vhd
@@ -1,35 +1,27 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 26.11.2022 16:57:34
--- Design Name: 
+-- Company:
+-- Engineer:
+--
+-- Create Date: 23.11.2022 16:50:19
+-- Design Name:
 -- Module Name: Test_Bench - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
--- 
+--
 ----------------------------------------------------------------------------------
 
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+use ieee.numeric_std.all;
 
 entity Test_Bench is
 --  Port ( );
@@ -37,7 +29,46 @@ end Test_Bench;
 
 architecture Behavioral of Test_Bench is
 
+  component Reg_Des
+    generic(n : integer := 8);
+    port(
+      d          : in  std_logic;
+      q          : out std_logic_vector(n-1 downto 0);
+      reset, des : in  std_logic;
+      clk        : in  std_logic);
+  end Component Reg_Des;
+
+  constant n : integer := 3;
+  constant semiperiodo : time := 10 ns;
+  signal d_interno, des_interno, reset_interno: std_logic := 'U';
+  signal q_interno : std_logic_vector(n-1 downto 0) := (others => 'U');
+  signal clk_interno : std_logic := '0';
+
 begin
 
+  DUT : Reg_Des
+    generic map (n)
+    port map(
+      d  => d_interno,
+      q  => q_interno,
+      reset  => reset_interno,
+      des => des_interno,
+      clk => clk_interno);
 
+-- Taken from The Student Guide to VHDL, Peter J.Asheden
+  clock_gen: process (clk_interno) is
+  begin
+    if clk_interno = '0' then
+      clk_interno <= '1' after semiperiodo,
+                     '0' after 2*semiperiodo;
+    end if;
+  end process clock_gen;
+
+  des_interno <= '1';
+
+  test: process
+  begin
+    d_interno <= '1' after 5 ns, '0' after 10 ns;
+
+  end process;
 end Behavioral;
--- ejercicios_basicos_vhdl/registro_desplazamiento/registro_desplazamiento.srcs/sources_1/new/Reg_Des.vhd
+++ ejercicios_basicos_vhdl/registro_desplazamiento/registro_desplazamiento.srcs/sources_1/new/Reg_Des.vhd
@@ -1,43 +1,51 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 26.11.2022 16:57:10
--- Design Name: 
--- Module Name: Reg_Des - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
+-- Company:
+-- Engineer:
+--
+-- Create Date: 26.11.2022 16:14:50
+-- Design Name:
+-- Module Name: MuxV_2a1 - Behavioral
+-- Project Name:
+-- Target Devices:
+-- Tool Versions:
+-- Description:
+--
+-- Dependencies:
+--
 -- Revision:
 -- Revision 0.01 - File Created
 -- Additional Comments:
--- 
+--
 ----------------------------------------------------------------------------------
 
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
+library ieee;
+use ieee.std_logic_1164.all;
 
 entity Reg_Des is
---  Port ( );
+  generic(n : integer := 8);
+  port(
+    d          : in  std_logic;
+    q          : out std_logic_vector(n-1 downto 0);
+    reset, des : in  std_logic;
+    clk        : in  std_logic);
 end Reg_Des;
 
-architecture Behavioral of Reg_Des is
-
+architecture A of Reg_Des is
+  signal temp : std_logic_vector(n-1 downto 0);
 begin
-
-
-end Behavioral;
+  process(clk, reset)
+  begin
+    if reset = '1' then
+      temp <= (others => '0');
+    elsif rising_edge(clk) then
+      if des = '1' then
+        for i in temp'high downto 1 loop
+          -- 'high Atributo para detectar el indice mayor de un array
+          temp(i) <= temp(i-1);
+        end loop;
+        temp(0) <= d;
+      end if;
+    end if;
+  end process;
+  q <= temp;
+end A;
