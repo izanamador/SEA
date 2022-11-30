b3ea3d7706c690895fa42f5b730c1f0fc67bc1e2
izanamador <izan.amador@gmail.com>
2022-11-30 11:48:25 +0100

    analog discovery testable top structure contador

---
+++ ejercicios_basicos_vhdl/contador_ascendente/contador_ascendente.srcs/sim_1/new/Test_Bench_top.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 30.11.2022 11:45:04
+-- Design Name: 
+-- Module Name: Test_Bench_top - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Test_Bench_top is
+--  Port ( );
+end Test_Bench_top;
+
+architecture Behavioral of Test_Bench_top is
+
+begin
+
+
+end Behavioral;
