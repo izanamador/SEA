f4833db0fdaed8bcb214d1acc8403e1e77b5e863
izanamador <izan.amador@gmail.com>
2022-12-06 11:43:02 +0100

    detector secuencia mealy project structure created

---
+++ diseno_simulacion_fsm/mealy/detector_secuencia/detector_secuencia.srcs/sources_1/new/detector_secuencia.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 06.12.2022 11:39:15
+-- Design Name: 
+-- Module Name: detector_secuencia - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity detector_secuencia is
+--  Port ( );
+end detector_secuencia;
+
+architecture Behavioral of detector_secuencia is
+
+begin
+
+
+end Behavioral;
