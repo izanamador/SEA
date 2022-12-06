2927d8d324cce903b784db8d68218f18dfd38055
izanamador <izan.amador@gmail.com>
2022-12-06 10:49:34 +0100

    sumador tb csv structure created

---
+++ ejercicios_basicos_vhdl/sumador/sumador.srcs/sim_1/new/Test_Bench_Fichero.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 06.12.2022 10:48:49
+-- Design Name: 
+-- Module Name: Test_Bench_Fichero - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Test_Bench_Fichero is
+--  Port ( );
+end Test_Bench_Fichero;
+
+architecture Behavioral of Test_Bench_Fichero is
+
+begin
+
+
+end Behavioral;
